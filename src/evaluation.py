83a7366ecfbb69e956518c0498c290756638912f
Daniel Young <young.daniel@outlook.com>
2025-07-17 05:48:45 +0800

    Add saving of model and metrics

--- src/evaluation.py
+++ src/evaluation.py
@@ -2,7 +2,7 @@ from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_sc
 import numpy as np
 
 
-def evaluate_model(model, X_test: np.ndarray, y_test: np.ndarray) -> dict:
+def evaluate_model(model, X_test: np.ndarray, y_test: np.ndarray, **kwargs) -> dict:
     """
     Evaluates the performance of the trained model on the test dataset.
 
@@ -22,9 +22,29 @@ def evaluate_model(model, X_test: np.ndarray, y_test: np.ndarray) -> dict:
     f1 = f1_score(y_test, y_pred)
     # we don't need weights because its binary classification
 
-    return {
+    metrics = {
         "accuracy": accuracy,
         "precision": precision,
         "recall": recall,
         "f1_score": f1,
     }
+
+    save_evaluation_metrics(
+        metrics, filename=kwargs.get("filename", "reports/metrics.txt")
+    )
+    return metrics
+
+
+def save_evaluation_metrics(
+    metrics: dict, filename: str = "reports/metrics.txt"
+) -> None:
+    """
+    Saves the evaluation metrics to a file.
+
+    Parameters:
+    metrics (dict): The evaluation metrics to save.
+    filename (str): The name of the file to save the metrics.
+    """
+    with open(filename, "w") as file:
+        for key, value in metrics.items():
+            file.write(f"{key}: {value}\n")
--- src/model_training.py
+++ src/model_training.py
@@ -1,8 +1,11 @@
 import numpy as np
+import pickle
 from sklearn.naive_bayes import MultinomialNB
 
 
-def train_nb(X: np.ndarray, y: np.ndarray, alpha: float = 0.2) -> MultinomialNB:
+def train_nb(
+    X: np.ndarray, y: np.ndarray, alpha: float = 0.2, **kwargs
+) -> MultinomialNB:
     """
     Trains multinomial Naive Bayes classifier on provided features and labels.
 
@@ -15,4 +18,18 @@ def train_nb(X: np.ndarray, y: np.ndarray, alpha: float = 0.2) -> MultinomialNB:
     """
     model = MultinomialNB(alpha=alpha)
     model.fit(X, y)
+
+    save_model(model, filename=kwargs.get("filename", "models/model.pkl"))
     return model
+
+
+def save_model(model, filename: str = "models/model.pkl") -> None:
+    """
+    Saves the trained model to a file.
+
+    Parameters:
+    model (MultinomialNB): The trained Naive Bayes model.
+    filename (str): The name of the file to save the model.
+    """
+    with open(filename, "wb") as file:
+        pickle.dump(model, file, protocol=5)
